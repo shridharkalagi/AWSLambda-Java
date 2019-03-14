49c67414b579929fcb77ebec0450da6ec032808d
shridhar kalagi <shridharkalagi@gmail.com>
2019-03-14 12:02:07 +0530

    Adding necessary libs
    Add only the zip

--- src/test/java/com/blackboard/testing/tests/LambdaTest.java
+++ src/test/java/com/blackboard/testing/tests/LambdaTest.java
@@ -28,27 +28,27 @@ public class LambdaTest extends LambdaBaseTest {
     @Test
     public void googleTest1() {
         open("http://www.google.com/");
-        screenshot("google-home-page");
+        screenshot("google-home-page2");
         assertThat(title(), containsString("Google"));
     }
 
     @Test
     public void blackboardTest2() {
         open("http://www.blackboard.com/");
-        screenshot("blackboard-home-page");
+        screenshot("blackboard-home-page2");
         assertThat(title(), containsString("Blackboard"));
     }
     @Test
     public void googleTest3() {
         open("http://www.google.com/");
-        screenshot("google-home-page");
+        screenshot("google-home-page4");
         assertThat(title(), containsString("Google"));
     }
 
     @Test
     public void blackboardTest3() {
         open("http://www.blackboard.com/");
-        screenshot("blackboard-home-page");
+        screenshot("blackboard-home-page4");
         assertThat(title(), containsString("Blackboard"));
     }
 }
